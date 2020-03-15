ad1ef01380c3e1dc487ada4755c9c75333db21aa
yoonwoo123 <lkkjasd@korea.ac.kr>
2020-03-16 00:35:40 +0900

    200315프로 - 백준 회의실배정 코드 약간 수정

--- 200315프로/회의실배정.py
+++ 200315프로/회의실배정.py
@@ -7,7 +7,7 @@ meetings = [(*map(int, input().split()),) for _ in range(N)]
 meetings.sort()
 ans = 1
 stime = meetings[-1][0]
-for i in range(len(meetings)-2, -1, -1):
+for i in range(N-2, -1, -1):
     if stime >= meetings[i][1]:
         ans += 1
         stime = meetings[i][0]
