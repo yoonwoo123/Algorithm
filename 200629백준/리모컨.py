434d1d34cb58259e8730ddf5e50809489cd4a731
YoonWoo <lkkjasd@korea.ac.kr>
2020-06-30 23:10:17 +0900

    200629백준 - 리모컨
    - 답이 틀린 이유 중 하나는 자릿 수가 바뀔 때 못잡아내는 것이므로 4자릿수 수를 찾을 때 가장 큰 3자릿수, 가장 작은 5자릿수를 고려해보는 방식으로 짜보자

--- 200629백준/리모컨.py
+++ 200629백준/리모컨.py
@@ -5,19 +5,24 @@ sys.stdin = open("리모컨_input.txt")
 # 만든수의 길이 + 만든 수와 답의 차이를 더해주면 답
 
 target = int(input())
+TN = len(str(target))
 remocon = set(x for x in range(10))
 N = int(input())
 if N == 10: # 전부 고장났으면 무조건 target - 100 이 답
     print(abs(target - 100))
 else:
     if N == 0:
-        print(min(abs(target - 100), len(str(target))))
+        print(min(abs(target - 100), TN))
     else:
         remocon = remocon - set(map(int, input().split()))
         # 순열로 만든 모든 수 중 target과 차이가 가장 적은 수 찾기
         ans = 999999
-        for perm in itertools.product(remocon, repeat= len(str(target))):
-            if ans == len(str(target)): break
-            num = int(''.join(map(str, perm)))
-            ans = min(ans, abs(target - num) + len(str(num)))
+        # 답이 틀릴 수 있는 반례의 경우 생각해보면 자릿수가 바뀔 때
+        # 못잡아준다. 그래서 4자릿수라 가정 시 만들 수 있는
+        # 가장 큰 3자릿 수 만들수 있는 가장 작은 5자릿수를 염두하자.
+        for i in range(TN-1, len(str(target))+2):
+            for perm in itertools.product(remocon, repeat= 9):
+                if ans == len(str(target)): break
+                num = int(''.join(map(str, perm)))
+                ans = min(ans, abs(target - num) + len(str(num)))
         print(min(abs(target - 100), ans))
