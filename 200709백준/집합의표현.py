8d99bdc8132b8d6a31792e7c1d50b543b3c99cb1
YoonWoo <lkkjasd@korea.ac.kr>
2020-07-11 09:31:46 +0900

    200709백준 - 집합의표현
    - G4 문제로 union - find 알고리즘(disjoint-set) 을 이용하여 풀었다. N, M이 100만 10만으로 무척 크기 때문에 통상적인 방법으로는 시간이 초과될 문제

--- 200709백준/집합의표현.py
+++ 200709백준/집합의표현.py
@@ -1,4 +1,35 @@
 import sys
 sys.stdin = open("집합의표현_input.txt")
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
+
+# 유니온 파인드로 합집합을 만들땐 뿌리부모를 같은걸로 해주고
+# 같은 집합에 포함되있나 검사는 부모가 같다면 yes 아니면 no
+
+def parent_find(x):
+    if parents[x] == x:
+        return parents[x]
+    parents[x] = parent_find(parents[x])
+    return parents[x]
+
+def union(a, b):
+    a = parent_find(a)
+    b = parent_find(b)
+    parents[b] = a
+
+parents = [x for x in range(N+1)]
+for i in range(M):
+    NO, A, B = map(int, input().split())
+    if NO == 0:
+        if A != B:
+            union(A, B)
+    else:
+        if A == B:
+            print('YES')
+        else:
+            if parent_find(A) == parent_find(B):
+                # print를 조금 더 빠르게 해주는 것
+                sys.stdout.write("YES\n")
+            else:
+                sys.stdout.write("NO\n")
